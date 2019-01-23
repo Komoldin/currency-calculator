25ef8ac4f560c1e36f5c7e8b42b0c445b3311706
Anna Kova <m3ta007@gmail.com>
2019-01-23 16:10:35 -0500

    API integrated

--- README.md
+++ README.md
@@ -1,9 +1,65 @@
-# Currency Calculator
+# Currency Calculator (beta)
 
-Currency Converter App helps users to convert USD into another usable currency (RUB) in just a moment. Based on current market exchange rates, app will calculate the currency's corresponding value of the entered amount of money.
+Currency Converter App helps users to convert USD into another usable currency (RUB, CAD, or EUR) in just a moment. Based on current market exchange rates, app will calculate the currency's corresponding value of the entered amount of money.
 
 ## Features:
 - AJAX requests;
-- Local JSON server with stored exchange rate and update date;
+- JSON API for live exchange rates updates;
 - Cticky footer;
-- HTML5, CSS3, JS(ES6)
+- HTML5, CSS3, JS(ES6).
+
+## Updates in v1.0 1/23/2019:
+- JSON API integrated as live JSON exchange rates server;
+- Localization realized for users to display the time that the rates were published;
+- UNIX Timestamp value used for DateTime manipulation;
+- Ruble sign converted in HTML Entity to improve cross-browser support;
+- Date script in copyright field fixed.
+
+## Updates in beta 1/18/2019:
+- Conversion expanded to CAD & EUR;
+- Support to all directions of conversion realized;
+- Fonts fixed;
+- Icon & flexbox added;
+- Art direction & resolution switching implemented;
+- Updated local JSON server with stored exchange rate and update date;
+- Optimized image breakpoints for background image introduced.
+
+## GTBD:
+- store JSON data from API to faster access it after the page is loaded and reduce requests to server (currently x5: repeated for each currency on input)
+- optimize image breakpoints for retina displays;
+- fix error message (after logical operator in requests)
+
+
+
+
+
+
+/* Image Breakpoints */
+
+/* 1.25 dpr */
+@media 
+(-webkit-min-device-pixel-ratio: 1.25), 
+(min-resolution: 120dpi){ 
+    /* Retina-specific stuff here */
+}
+
+/* 1.3 dpr */
+@media 
+(-webkit-min-device-pixel-ratio: 1.3), 
+(min-resolution: 124.8dpi){ 
+    /* Retina-specific stuff here */
+}
+
+/* 1.5 dpr */
+@media 
+(-webkit-min-device-pixel-ratio: 1.5), 
+(min-resolution: 144dpi){ 
+    /* Retina-specific stuff here */
+}
+
+ /* 2.0 dpr */
+@media 
+(-webkit-min-device-pixel-ratio: 2), 
+(min-resolution: 192dpi) { 
+    /* Retina-specific stuff here */
+}
--- js/script.js
+++ js/script.js
@@ -1,34 +1,58 @@
-let inputRub = document.querySelector('#RUB'),
-    inputUsd = document.querySelector('#USD'),
+let inputUsd = document.querySelector('#usd'),
+    inputRub = document.querySelector('#rub'),
+    inputEur = document.querySelector('#eur'),
+    inputCad = document.querySelector('#cad'),
     display = document.querySelector('.display'),
-    dateDisplay = document.querySelector('#date'),
-    rateRub = document.querySelector('.rateRub');
+    dateDisplay = document.querySelector('#timestamp'),
+    rateRub = document.querySelector('.rateRub'),
+    rateEur = document.querySelector('.rateEur'),
+    rateCad = document.querySelector('.rateCad');
 
 init();
 
 function init() {
-    getRate();
-    setInput();
+    getRates();
+    setInputUsd();
+    setInputRub();
+    setInputEur();
+    setInputCad();
 }
 
-function getRate() {
+function getRates() {
     let request = new XMLHttpRequest();
     
-    request.open('GET', 'js/current.json');
+    request.open('GET', 'https://openexchangerates.org/api/latest.json?app_id=23ac26e56c81446ea3b56f485c0aaa56&symbols=RUB,EUR,CAD'); // live server API
+    // request.open('GET', 'js/current.json'); // local server
     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     request.send();
 
     request.addEventListener('readystatechange', function() {
         if (request.readyState == 4 && request.status == 200) {
-            let data = JSON.parse(request.response);
+            const data = JSON.parse(request.response);
 
-            rateRub.textContent = data.usd;
-            dateDisplay.textContent = `${data.dateMM}.${data.dateDD}.${data.dateYYYY}`;
+            rateRub.textContent = data.rates.RUB.toFixed(2);
+            rateEur.textContent = data.rates.EUR.toFixed(2);
+            rateCad.textContent = data.rates.CAD.toFixed(2);
+
+            let timestamp = new Date(data.timestamp*1000);
+            let date = new Date(timestamp).toLocaleString(undefined, {
+                day: 'numeric',
+                month: 'long',
+                year: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit'
+            });
+
+            dateDisplay.textContent = `${date}`;
+
+            // return data;
+        } else if (request.status != 200) {
+            showError();
         }
     });
 }
 
-function setInput() {
+function setInputRub() {
     inputRub.addEventListener('input', () => {
         let request = new XMLHttpRequest();
         
@@ -37,12 +61,92 @@ function setInput() {
         request.send();
         
         request.addEventListener('readystatechange', function() {
-            if (request.readyState === 4 && request.status == 200) {
+            if (request.readyState === 4 && request.status === 200) {
                 let data = JSON.parse(request.response);
+                let replyUsd = parseFloat(inputRub.value) / data.rub;
+                let replyEur = parseFloat(inputRub.value) / data.rub * data.eur;
+                let replyCad = parseFloat(inputRub.value) / data.rub * data.cad;
                 
-                inputUsd.value = Number.parseFloat(inputRub.value / data.usd).toFixed(2);
-            } else {
-                inputUsd.value = 'Something went wrong';
+                inputUsd.value = replyUsd.toFixed(2);
+                inputEur.value = replyEur.toFixed(2);
+                inputCad.value = replyCad.toFixed(2);
+            } else if (request.status != 200) {
+                showError();
+            }
+        });
+    });
+}
+
+function setInputUsd() {
+    inputUsd.addEventListener('input', () => {
+        let request = new XMLHttpRequest();
+        
+        request.open('GET', 'js/current.json');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        request.send();
+        
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState === 4 && request.status === 200) {
+                let data = JSON.parse(request.response);
+                let replyRub = parseFloat(inputUsd.value) * data.rub;
+                let replyEur = parseFloat(inputUsd.value) * data.eur;
+                let replyCad = parseFloat(inputUsd.value) * data.cad;
+                
+                inputRub.value = replyRub.toFixed(2);
+                inputEur.value = replyEur.toFixed(2);
+                inputCad.value = replyCad.toFixed(2);
+            } else if (request.status != 200) {
+                showError();
+            }
+        });
+    });
+}
+
+function setInputEur() {
+    inputEur.addEventListener('input', () => {
+        let request = new XMLHttpRequest();
+        
+        request.open('GET', 'js/current.json');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        request.send();
+        
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState === 4 && request.status === 200) {
+                let data = JSON.parse(request.response);
+                let replyUsd = parseFloat(inputEur.value) / data.eur;
+                let replyRub = parseFloat(inputEur.value) / data.eur * data.rub;
+                let replyCad = parseFloat(inputEur.value) / data.eur * data.cad;
+                
+                inputUsd.value = replyUsd.toFixed(2);
+                inputRub.value = replyRub.toFixed(2);
+                inputCad.value = replyCad.toFixed(2);
+            } else if (request.status != 200) {
+                showError();
+            }
+        });
+    });
+}
+
+function setInputCad() {
+    inputCad.addEventListener('input', () => {
+        let request = new XMLHttpRequest();
+        
+        request.open('GET', 'js/current.json');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        request.send();
+        
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState === 4 && request.status === 200) {
+                let data = JSON.parse(request.response);
+                let replyUsd = parseFloat(inputCad.value) / data.cad;
+                let replyEur = parseFloat(inputCad.value) / data.cad * data.eur;
+                let replyRub = parseFloat(inputCad.value) / data.cad * data.rub;
+                
+                inputUsd.value = replyUsd.toFixed(2);
+                inputRub.value = replyRub.toFixed(2);
+                inputEur.value = replyEur.toFixed(2);
+            } else if (request.status != 200) {
+                showError();
             }
         });
     });
@@ -53,5 +157,20 @@ function showError() {
     display.style.color = 'var(--accent)';
     display.style.fontWeight = '600';
     display.style.fontSize = '110%';
-    // display.style.textTransform = 'uppercase';
 }
+
+
+// Copyright Date Script
+let dates = document.querySelector('span#date');
+
+if (new Date().getFullYear()>2019) {
+    let date = new Date().getFullYear();
+    dates.textContent = `-${date}`;
+}
+
+// Global site tag (gtag.js) - Google Analytics Script
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-131142493-1');
